af5f9f6c58b12b5711c5d93b9bc1bce687cb315f
Tim Dykes <ttdykes@tesseract.office.ictn.com.au>
2013-11-08 13:22:12 +1100

    Added hostname, cpu load, and free disk. Move temp to second page. Changed update time to 10 seconds to benefit multiple pages.

--- examples/sysinfo.py
+++ examples/sysinfo.py
@@ -4,27 +4,37 @@ import subprocess
 from time import sleep
 import pifacecad
 
-
-UPDATE_INTERVAL = 60 * 5  # 5 mins
+SHUTDOWN_HOLD = 0
+UPDATE_INTERVAL = 10  # 10 sec
 GET_IP_CMD = "hostname --all-ip-addresses"
 GET_TEMP_CMD = "/opt/vc/bin/vcgencmd measure_temp"
 TOTAL_MEM_CMD = "free | grep 'Mem' | awk '{print $2}'"
 USED_MEM_CMD = "free | grep '\-\/+' | awk '{print $3}'"
+CPU_LOAD_CMD = "uptime | awk '{print $9}' | sed s/,/""/g"
+HOSTNAME_CMD = "hostname"
+FREE_DISK_CMD = "df -h | grep rootfs | awk '{print $5}'"
 
-temperature_symbol = pifacecad.LCDBitmap(
-    [0x4, 0x4, 0x4, 0x4, 0xe, 0xe, 0xe, 0x0])
-memory_symbol = pifacecad.LCDBitmap(
-    [0xe, 0x1f, 0xe, 0x1f, 0xe, 0x1f, 0xe, 0x0])
-temp_symbol_index, memory_symbol_index = 0, 1
+cpu_symbol = pifacecad.LCDBitmap( [0x0, 0x1f, 0x11, 0x15, 0x11, 0x1f, 0x0, 0x0] )
+disk_symbol = pifacecad.LCDBitmap( [0x0, 0x1c, 0x1e, 0x1e, 0x1e, 0x1e, 0x0, 0x0] )
+temperature_symbol = pifacecad.LCDBitmap( [0x4, 0x4, 0x4, 0x4, 0xe, 0xe, 0xe, 0x0] )
+memory_symbol = pifacecad.LCDBitmap( [0xe, 0x1f, 0xe, 0x1f, 0xe, 0x1f, 0xe, 0x0] )
 
+temp_symbol_index, memory_symbol_index, cpu_symbol_index, disk_symbol_index = 0, 1, 2, 3
 
 def run_cmd(cmd):
     return subprocess.check_output(cmd, shell=True).decode('utf-8')
 
+def get_cpu_load():
+    return run_cmd(CPU_LOAD_CMD)[:-1]
+
+def get_disk_usage():
+    return run_cmd(FREE_DISK_CMD)[:-1]
 
 def get_my_ip():
     return run_cmd(GET_IP_CMD)[:-1]
 
+def get_hostname():
+    return run_cmd(HOSTNAME_CMD)[:-1]
 
 def get_my_temp():
     return run_cmd(GET_TEMP_CMD)[5:9]
@@ -48,12 +58,20 @@ def show_sysinfo():
     while True:
         cad.lcd.clear()
         cad.lcd.write("IP:{}\n".format(get_my_ip()))
-
-        cad.lcd.write_custom_bitmap(temp_symbol_index)
-        cad.lcd.write(":{}C ".format(get_my_temp()))
-
+        cad.lcd.write_custom_bitmap(cpu_symbol_index)
+        cad.lcd.write(":{} ".format(get_cpu_load()))
         cad.lcd.write_custom_bitmap(memory_symbol_index)
         cad.lcd.write(":{}".format(get_my_free_mem()))
+
+        sleep(UPDATE_INTERVAL)
+
+        cad.lcd.clear()
+        cad.lcd.write(format(get_hostname())+"\n")
+        cad.lcd.write_custom_bitmap(temp_symbol_index)
+        cad.lcd.write(":{}C ".format(get_my_temp()))        
+        cad.lcd.write_custom_bitmap(disk_symbol_index)
+        cad.lcd.write(":{}".format(get_disk_usage()))
+
         sleep(UPDATE_INTERVAL)
 
 
@@ -69,6 +87,8 @@ if __name__ == "__main__":
     else:
         cad.lcd.store_custom_bitmap(temp_symbol_index, temperature_symbol)
         cad.lcd.store_custom_bitmap(memory_symbol_index, memory_symbol)
+        cad.lcd.store_custom_bitmap(cpu_symbol_index, cpu_symbol)
+        cad.lcd.store_custom_bitmap(disk_symbol_index, disk_symbol)
         cad.lcd.backlight_on()
         cad.lcd.write("Waiting for IP..")
         wait_for_ip()
